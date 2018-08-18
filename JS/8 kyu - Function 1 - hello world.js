a414edc5b50c87b3796892e3a09dce592c96f55a
栗佐さん <40574176+XxDan10xX@users.noreply.github.com>
2018-08-18 20:19:51 +0300

    Create 8 kyu - Function 1 - hello world.js

---
+++ JS/8 kyu - Function 1 - hello world.js
@@ -0,0 +1,32 @@
+function greet(){
+			var a = ['h', 'e', '1', '1'],
+			x = 'o',
+			u = !function(a, t) {
+				a.push(t);
+			}(a, x),
+			f = 2,
+			_f = 3,
+			t = 'l',
+			r = !function(f, _f, t) {
+				a[f] = a[_f] = t;
+			}(f, _f, t),
+			_a = ['w', '0', 'r', 'l', 'd'],
+			_x = '!',
+			p = !function(a, t) {
+				a.push(t);
+			}(_a, _x),
+			h = ' ',
+			k = !function(a, t) {
+				a.unshift(t);
+			}(_a, h),
+			__f = 2,
+			_t = 'o',
+			_r = !function(f, t) {
+				_a[f] = t;
+			}(__f, _t),
+			n = [],
+			c = !function(a, _a) {
+				n = a.concat(_a);
+			}(a, _a);
+			return n.join('');
+		}
