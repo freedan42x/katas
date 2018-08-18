a600d16d7607753e2c86a28ad6651c2709162343
栗佐さん <40574176+XxDan10xX@users.noreply.github.com>
2018-08-18 15:17:59 +0300

    Create 8 kyu - Switch it Up!.js

---
+++ JS/8 kyu - Switch it Up!.js
@@ -0,0 +1,9 @@
+const switchItUp = n => n == 1 ? 'One' : n == 2 
+                               ? 'Two' : n == 3
+                               ? 'Three' : n == 4 
+                               ? 'Four' : n == 5 
+                               ? 'Five' : n == 6 
+                               ? 'Six' : n == 7  ? 
+                               'Seven' : n == 8
+                               ? 'Eight' : n == 9 
+                               ? 'Nine' : 'Zero';
