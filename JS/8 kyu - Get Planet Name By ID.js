42ba2630c3a4659d5a937d41783c5beed9996430
栗佐さん <40574176+XxDan10xX@users.noreply.github.com>
2018-08-22 22:07:44 +0300

    Create 8 kyu - Get Planet Name By ID.js

---
+++ JS/8 kyu - Get Planet Name By ID.js
@@ -0,0 +1,8 @@
+const getPlanetName = id => id == 1 ? 'Mercury' :
+                            id == 2 ? 'Venus'   :
+                            id == 3 ? 'Earth'   :
+                            id == 4 ? 'Mars'    :
+                            id == 5 ? 'Jupiter' :
+                            id == 6 ? 'Saturn'  :
+                            id == 7 ? 'Uranus'  :
+                                      'Neptune' ;
