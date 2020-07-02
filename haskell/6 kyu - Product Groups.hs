58298f95df0bee714fbdf57512a6392233c506e6
Bredor <40574176+freedan42x@users.noreply.github.com>
2020-07-02 16:07:32 +0300

    Create 6 kyu - Product Groups.hs

---
+++ haskell/6 kyu - Product Groups.hs
@@ -0,0 +1,12 @@
+module CodeWars.Group.Product where
+import CodeWars.Group
+import Data.Monoid
+
+instance (Group a, Group b) => Group (a, b) where
+  invert (a, b) = (invert a, invert b)
+
+embedLeft :: (Group a, Group b) => a -> (a,b)
+embedLeft a = (a, mempty)
+
+embedRight :: (Group a, Group b) => b -> (a,b)
+embedRight b = (mempty, b)
