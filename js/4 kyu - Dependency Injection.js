97d36d3536d2dfaf7a192bc165186cc8d89e0b3a
Bredor <40574176+freedan42x@users.noreply.github.com>
2022-07-18 23:22:28 +0300

    Create 4 kyu - Dependency Injection.js

---
+++ js/4 kyu - Dependency Injection.js
@@ -0,0 +1,22 @@
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var ARGUMENT_NAMES = /([^\s,]+)/g;
+function getParamNames(func) {
+  var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+  var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+  if(result === null)
+     result = [];
+  return result;
+}
+
+/**
+ * Constructor DependencyInjector
+ * @param {Object} - object with dependencies
+ */
+var DI = function (dependency) {
+  this.dependency = dependency;
+};
+
+// Should return new function with resolved dependencies
+DI.prototype.inject = function (func) {
+  return () => func(...getParamNames(func).map(name => this.dependency[name]));
+}
